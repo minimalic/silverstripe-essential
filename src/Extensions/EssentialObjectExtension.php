b4c8c60d4547608e18ff8fd9b335a6fc9d9e2851
minimalic <git@minimalic.com>
2024-05-22 01:25:30 +0200

    Add methods for converting Arrays and Class Names

--- src/Extensions/EssentialObjectExtension.php
+++ src/Extensions/EssentialObjectExtension.php
@@ -3,6 +3,8 @@
 namespace minimalic\Essential\Extensions;
 
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Core\Convert;
+// use SilverStripe\Core\ClassInfo;
 
 /**
  * Extension for the "DataObject" class.
@@ -13,10 +15,60 @@ use SilverStripe\ORM\DataExtension;
 class EssentialObjectExtension extends DataExtension
 {
 
-    public function GlobalFunction()
+    /**
+     * Converts ArrayList to Array
+     *
+     * @param ArrayList
+     *
+     * @return Array
+     */
+    public static function ArrayListToArray($arrayList)
+    {
+        $array = [];
+        foreach ($arrayList as $item) {
+            $array[$item->Key] = $item->Value;
+        }
+        return $array;
+    }
+
+    public static function ConvertToSegment($title) {
+        // $filter = URLSegmentFilter::create();
+        // $filter->setReplacements(['/[\/]/' => '-']);
+        // print_r($filter->getReplacements());
+        // $segment = $filter->filter($title);
+        $title = preg_replace('/\//', '-', $title);
+        $segment = Convert::raw2url($title);
+
+        return $segment;
+    }
+
+    // public static function ShortClassName($class)
+    // {
+    //     $shortName = ClassInfo::shortName($class);
+    //     return $shortName;
+    // }
+
+    /**
+     * Generates url-friendly class name
+     *
+     * @return String
+     */
+    public function getClassNameForTemplate()
     {
         $className = (new \ReflectionClass($this->owner))->getShortName();
-        return 'Hello, ' . $className;
+        $className = $this->ConvertToSegment($className);
+        return $className;
+    }
+
+    /**
+     * Generates url-friendly class name with ID number
+     *
+     * @return String
+     */
+    public function getClassNameIDForTemplate()
+    {
+        $className = $this->getClassNameForTemplate() . '-' . $this->owner->ID;
+        return $className;
     }
 
 }
